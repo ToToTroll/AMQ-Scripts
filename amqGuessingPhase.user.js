6041fba549c248447db3d20f84bb9446a5d1903c
ToToTroll <totomaniaracer@gmail.com>
2024-10-05 13:39:31 +0200

    Added Ctrl+Q to toggle script

--- amqGuessingPhase.user.js
+++ amqGuessingPhase.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AMQ Guessing Phase
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Sends messages whenever guessing phase starts and ends. Useful for moderating ranked chat.
+// @version      1.1
+// @description  Sends messages whenever guessing phase starts and ends. Useful for moderating ranked chat. Ctrl+Q to toggle.
 // @author       ToToTroll
 // @match        https://*.animemusicquiz.com/*
 // @grant        none
@@ -10,6 +10,10 @@
 // @updateURL    https://github.com/ToToTroll/AMQ-Scripts/raw/refs/heads/master/amqGuessingPhase.user.js
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js
 // ==/UserScript==
+
+//disabled by default
+let enabled = false;
+
 if (document.getElementById('startPage')) return;
 
 // Wait until the LOADING... screen is hidden and load script
@@ -20,25 +24,39 @@ let loadInterval = setInterval(() => {
     }
 }, 500);
 
+//Toggle on/off
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 'q') {
+        event.preventDefault();
+        enabled = !enabled;
+        if(enabled) gameChat.systemMessage(`Guessing Phase messages enabled!`);
+        else gameChat.systemMessage(`Guessing Phase messages disabled!`);
+    }
+});
+
+function message(msg) {
+    if (enabled) gameChat.systemMessage(msg);
+}
+
 //Initialize listeners and 'Installed Userscripts' menu
 function setup() {
     new Listener("Game Starting", () => {
-        gameChat.systemMessage(`========== Game Start ==========`)
+        message(`========== Game Start ==========`)
     }).bindListener();
     new Listener("play next song", () => {
-        gameChat.systemMessage(`========== Round ` +  (parseInt($("#qpCurrentSongCount").text()) + 1) + ` - Guessing Phase Start ==========`)
+        message(`========== Round ` +  (parseInt($("#qpCurrentSongCount").text()) + 1) + ` - Guessing Phase Start ==========`)
     }).bindListener();
     new Listener("guess phase over", () => {
-        gameChat.systemMessage(`========== Round ` +  (parseInt($("#qpCurrentSongCount").text())) + ` - Guessing Phase End ==========`)
+        message(`========== Round ` +  (parseInt($("#qpCurrentSongCount").text())) + ` - Guessing Phase End ==========`)
     }).bindListener();
     new Listener("quiz end result", () => {
-        gameChat.systemMessage(`========== Game End ==========`)
+        message(`========== Game End ==========`)
     }).bindListener();
     AMQ_addScriptData({
         name: "AMQ Guessing Phase",
         author: "ToToTroll",
-        version: "1.0",
+        version: "1.1",
         link: "https://github.com/ToToTroll/AMQ-Scripts/raw/refs/heads/master/amqGuessingPhase.user.js",
-        description: `Sends messages whenever guessing phase starts and ends. Useful for moderating ranked chat.`
+        description: `Sends messages whenever guessing phase starts and ends. Useful for moderating ranked chat. Ctrl+Q to toggle.`
     });
 }
